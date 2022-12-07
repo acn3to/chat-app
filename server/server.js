4d3f956315ca5f944927450a2c25f0b4ce811069
acn3to <costanetoow@gmail.com>
2022-12-07 16:02:53 -0300

    :sparkles: feat: starts an express.js server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+const rooms = ['front-end', 'back-end']
+const cors = require('cors')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+
+const server = require('http').createServer(app)
+const PORT = 5001
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'https://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+})
+
+server.listen(PORT, () => {
+  console.log('listening to port', PORT)
+})
