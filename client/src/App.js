32c9dde4c2342637ec997442312005b5eb3cc01e
acn3to <costanetoow@gmail.com>
2022-12-08 11:26:19 -0300

    :sparkles: feat: set chat states and contexts

--- client/src/App.js
+++ client/src/App.js
@@ -8,25 +8,52 @@ import Chat from './pages/Chat'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import { useState } from 'react'
+import { AppContext, socket } from './context/appContext'
 
 function App() {
+  const [rooms, setRooms] = useState([])
+  const [currentRoom, setCurrentRoom] = useState([])
+  const [members, setMembers] = useState([])
+  const [messages, setMessages] = useState([])
+  const [privateMemberMsg, setPrivateMemberMsg] = useState({})
+  const [newMessages, setNewMessages] = useState({})
   const user = useSelector((state) => state.user)
+
   return (
-    <div>
-      <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {!user && (
-            <>
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-            </>
-          )}
-          <Route path="/chat" element={<Chat />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <AppContext.Provider
+      value={{
+        socket,
+        currentRoom,
+        setCurrentRoom,
+        members,
+        setMembers,
+        messages,
+        setMessages,
+        privateMemberMsg,
+        setPrivateMemberMsg,
+        rooms,
+        setRooms,
+        newMessages,
+        setNewMessages,
+      }}
+    >
+      <div>
+        <BrowserRouter>
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {!user && (
+              <>
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+              </>
+            )}
+            <Route path="/chat" element={<Chat />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </AppContext.Provider>
   )
 }
 
