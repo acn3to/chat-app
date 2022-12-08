e559417aa7f42412a016d8ee02909903c437833e
acn3to <costanetoow@gmail.com>
2022-12-08 11:28:43 -0300

    :sparkles: feat: handle message form actions

--- client/src/components/MessageForm.js
+++ client/src/components/MessageForm.js
@@ -1,32 +1,140 @@
-import React from 'react'
-import { Form, Row, Col, FormGroup, FormControl, Button } from 'react-bootstrap'
+import React, { useContext, useEffect, useRef, useState } from 'react'
+import { Button, Col, Form, Row } from 'react-bootstrap'
 import { useSelector } from 'react-redux'
+import { AppContext } from '../context/appContext'
 import './MessageForm.css'
+function MessageForm() {
+  const [message, setMessage] = useState('')
+  const user = useSelector((state) => state.user)
+  const {
+    socket,
+    currentRoom,
+    setMessages,
+    messages,
+    privateMemberMsg,
+  } = useContext(AppContext)
+  const messageEndRef = useRef(null)
+  useEffect(() => {
+    scrollToBottom()
+  }, [messages])
 
-const MessageForm = () => {
+  function getFormattedDate() {
+    const date = new Date()
+    const year = date.getFullYear()
+    let month = (1 + date.getMonth()).toString()
+
+    month = month.length > 1 ? month : '0' + month
+    let day = date.getDate().toString()
+
+    day = day.length > 1 ? day : '0' + day
+
+    return month + '/' + day + '/' + year
+  }
+
   function handleSubmit(e) {
     e.preventDefault()
   }
 
-  const user = useSelector((state) => state.user)
+  function scrollToBottom() {
+    messageEndRef.current?.scrollIntoView({ behavior: 'smooth' })
+  }
 
+  const todayDate = getFormattedDate()
+
+  socket.off('room-messages').on('room-messages', (roomMessages) => {
+    setMessages(roomMessages)
+  })
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (!message) return
+    const today = new Date()
+    const minutes =
+      today.getMinutes() < 10 ? '0' + today.getMinutes() : today.getMinutes()
+    const time = today.getHours() + ':' + minutes
+    const roomId = currentRoom
+    socket.emit('message-room', roomId, message, user, time, todayDate)
+    setMessage('')
+  }
   return (
     <>
       <div className="messages-output">
+        {user && !privateMemberMsg?._id && (
+          <div className="alert alert-info">
+            Você está na sala {currentRoom}
+          </div>
+        )}
+        {user && privateMemberMsg?._id && (
+          <>
+            <div className="alert alert-info conversation-info">
+              <div>
+                Sua conversa com {privateMemberMsg.name}
+                <img
+                  src={privateMemberMsg.picture}
+                  className="conversation-profile-pic"
+                />
+              </div>
+            </div>
+          </>
+        )}
         {!user && (
           <div className="alert alert-danger">Por favor, faça o login</div>
         )}
+
+        {user &&
+          messages.map(({ _id: date, messagesByDate }, idx) => (
+            <div key={idx}>
+              <p className="alert alert-info text-center message-date-indicator">
+                {date}
+              </p>
+              {messagesByDate?.map(
+                ({ content, time, from: sender }, msgIdx) => (
+                  <div
+                    className={
+                      sender?.email === user?.email
+                        ? 'message'
+                        : 'incoming-message'
+                    }
+                    key={msgIdx}
+                  >
+                    <div className="message-inner">
+                      <div className="d-flex align-items-center mb-3">
+                        <img
+                          src={sender.picture}
+                          style={{
+                            width: 35,
+                            height: 35,
+                            objectFit: 'cover',
+                            borderRadius: '50%',
+                            marginRight: 10,
+                          }}
+                        />
+                        <p className="message-sender">
+                          {sender._id === user?._id ? 'Você' : sender.name}
+                        </p>
+                      </div>
+                      <p className="message-content">{content}</p>
+                      <p className="message-timestamp-left">{time}</p>
+                    </div>
+                  </div>
+                ),
+              )}
+            </div>
+          ))}
+        <div ref={messageEndRef} />
       </div>
       <Form onSubmit={handleSubmit}>
         <Row>
           <Col md={11}>
-            <FormGroup>
-              <FormControl
+            <Form.Group>
+              <Form.Control
                 type="text"
                 placeholder="Sua mensagem"
                 disabled={!user}
-              ></FormControl>
-            </FormGroup>
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
           </Col>
           <Col md={1}>
             <Button
