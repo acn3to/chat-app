8a963fc0f1d7c9e27522972289d658f1b3fe7970
acn3to <costanetoow@gmail.com>
2022-12-10 09:55:58 -0300

    :boom: fix: corrects dotenv file path

--- server/connection.js
+++ server/connection.js
@@ -1,9 +1,7 @@
+require('dotenv').config({ path: '../.env' })
 const mongoose = require('mongoose')
-require('dotenv').config()
 
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.ozlxols.mongodb.net/?retryWrites=true&w=majority`,
-  )
+  .connect(process.env.DB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((e) => console.log(e))
